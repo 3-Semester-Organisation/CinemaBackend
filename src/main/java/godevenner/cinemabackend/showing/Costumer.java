614389b003141f7af3e537c04d9698fb3d69f267
Chris Ye <chye0001@kea.stud.dk>
2024-09-30 21:00:00 +0200

    Created JPA entity Costumer and made association to User

---
+++ src/main/java/godevenner/cinemabackend/showing/Costumer.java
@@ -0,0 +1,29 @@
+package godevenner.cinemabackend.showing;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Costumer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private String name;
+    private int phoneNumber;
+    private String email;
+    private LocalDate dateOfBirth;
+}
