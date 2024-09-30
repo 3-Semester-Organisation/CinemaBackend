f0227529cdabe78879f6384225c5312f3456ff21
Chris Ye <chye0001@kea.stud.dk>
2024-09-30 20:20:39 +0200

    made Movie a JPA entity

--- src/main/java/godevenner/cinemabackend/movie/Movie.java
+++ src/main/java/godevenner/cinemabackend/movie/Movie.java
@@ -1,8 +1,26 @@
 package godevenner.cinemabackend.movie;
 
 import godevenner.cinemabackend.enums.Genre;
+import godevenner.cinemabackend.showing.Showing;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
 public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @OneToMany(mappedBy = "movie")
+    private Set<Showing> showingSet;
 
     private String title;
     private Genre genre;
