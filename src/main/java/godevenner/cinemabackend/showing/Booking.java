6229a4a41186d7609dfe0264495751704dd28873
Chris Ye <chye0001@kea.stud.dk>
2024-09-30 20:47:54 +0200

    created JPA entity Booking, and connected it to Showing

---
+++ src/main/java/godevenner/cinemabackend/showing/Booking.java
@@ -0,0 +1,26 @@
+package godevenner.cinemabackend.showing;
+
+import godevenner.cinemabackend.enums.SeatType;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Booking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @OneToOne
+    @JoinColumn(name = "showing_id")
+    private Showing showing;
+
+    private int row;
+    private String seat;
+    private SeatType seatType;
+}
