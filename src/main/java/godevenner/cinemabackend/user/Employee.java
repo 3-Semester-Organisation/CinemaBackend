7a26ec44c65652a5b62eb86e98108f4d78b5ea2c
Chris Ye <chye0001@kea.stud.dk>
2024-09-30 21:07:27 +0200

    created JPA Entity Employee and made association to Employee

---
+++ src/main/java/godevenner/cinemabackend/user/Employee.java
@@ -0,0 +1,23 @@
+package godevenner.cinemabackend.user;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private String name;
+}
