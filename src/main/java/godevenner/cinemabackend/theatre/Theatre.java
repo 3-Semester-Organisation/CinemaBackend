a99b972bb568789206100d0127bded51fcc2e4d4
Chris Ye <chye0001@kea.stud.dk>
2024-09-30 20:39:23 +0200

    made Theatre a JPA Entity and connected it to TheatreLayout and Showing

---
+++ src/main/java/godevenner/cinemabackend/theatre/Theatre.java
@@ -0,0 +1,29 @@
+package godevenner.cinemabackend.theatre;
+
+import godevenner.cinemabackend.showing.Showing;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Theatre {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @OneToOne(mappedBy = "theatre")
+    private Showing showing;
+
+    @OneToOne
+    @JoinColumn(name = "theatre_layout_id")
+    private TheatreLayout theatreLayout;
+
+    private int noOfSeats;
+    private int rowSize;
+    private int rows;
+}
