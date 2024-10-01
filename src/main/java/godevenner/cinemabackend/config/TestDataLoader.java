16de77d9e795641d534c59627896dc56ef83867c
Lars <144362461+laha0006@users.noreply.github.com>
2024-10-01 11:29:49 +0200

    test data

---
+++ src/main/java/godevenner/cinemabackend/config/TestDataLoader.java
@@ -0,0 +1,13 @@
+package godevenner.cinemabackend.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+
+@RequiredArgsConstructor
+public class TestDataLoader implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
