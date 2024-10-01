5e58c99a49c50db254a76b80981e69efef43db54
Lars <144362461+laha0006@users.noreply.github.com>
2024-10-01 20:02:47 +0200

    added TheatreRepository

---
+++ src/main/java/godevenner/cinemabackend/theatre/TheatreRepository.java
@@ -0,0 +1,6 @@
+package godevenner.cinemabackend.theatre;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TheatreRepository extends JpaRepository<Theatre, Long> {
+}
