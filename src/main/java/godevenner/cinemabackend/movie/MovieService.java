aa7fd410672181dc54e7eeb430ccdedf41d0cde8
Christoffer <chri26eq@stud.kea.dk>
2024-10-01 16:20:43 +0200

    added MovieService { getAllMovies() getMoviesByAgeLimit() }

--- src/main/java/godevenner/cinemabackend/movie/MovieService.java
+++ src/main/java/godevenner/cinemabackend/movie/MovieService.java
@@ -1,4 +1,34 @@
 package godevenner.cinemabackend.movie;
 
+import godevenner.cinemabackend.showing.ShowingMapper;
+import godevenner.cinemabackend.showing.ShowingRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
 public class MovieService {
+
+    private final MovieRepository movieRepository;
+    private final MovieMapper movieMapper;
+
+    public MovieService(MovieRepository movieRepository, MovieMapper movieMapper) {
+        this.movieRepository = movieRepository;
+        this.movieMapper = movieMapper;
+    }
+
+    public Set<MovieDto> getAllMovies() {
+        List<Movie> movies = movieRepository.findAll();
+        return movies.stream().map(movieMapper).collect(Collectors.toSet());
+    }
+
+    public Set<MovieDto> getMoviesByAgeLimit (int maxAgeLimit) {
+        List<Movie> movies = movieRepository.findAll();
+        movies.removeIf(movie -> movie.getAgeLimit() > maxAgeLimit);
+        return movies.stream().map(movieMapper).collect(Collectors.toSet());
+    }
+
 }
