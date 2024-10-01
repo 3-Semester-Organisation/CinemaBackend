9d838593dd6721e1b8c267ea3b5ce22ea66ebcf2
Lars <144362461+laha0006@users.noreply.github.com>
2024-10-01 20:01:43 +0200

    added CustomerRepository

---
+++ src/main/java/godevenner/cinemabackend/costumer/CustomerRepository.java
@@ -0,0 +1,6 @@
+package godevenner.cinemabackend.costumer;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Costumer,Long> {
+}
