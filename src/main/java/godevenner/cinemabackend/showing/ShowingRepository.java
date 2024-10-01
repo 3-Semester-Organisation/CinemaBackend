e628498057db088faabed91c14c937046bf6006c
Chris Ye <chye0001@kea.stud.dk>
2024-10-01 09:49:37 +0200

    create ShowingRepository

---
+++ src/main/java/godevenner/cinemabackend/showing/ShowingRepository.java
@@ -0,0 +1,6 @@
+package godevenner.cinemabackend.showing;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ShowingRepository extends JpaRepository<Long, Showing> {
+}
