7053845e0906489ca66123302280ad210d9f8018
Christoffer <chri26eq@stud.kea.dk>
2024-10-02 08:33:08 +0200

    ændret getMovies() i MovieController

--- src/main/java/godevenner/cinemabackend/movie/MovieController.java
+++ src/main/java/godevenner/cinemabackend/movie/MovieController.java
@@ -16,19 +16,14 @@ public class MovieController {
         this.movieService = movieService;
     }
 
-    @GetMapping("/movies") //All Movies
-    public ResponseEntity<Set<MovieDto>> getAllMovies() {
-        Set<MovieDto> movies = movieService.getAllMovies();
+    @GetMapping("/movies")
+    public ResponseEntity<Set<MovieDto>> getMovies(@RequestParam(required = false) Integer age) {
+        Set<MovieDto> movies;
+        if (age == null) movies = movieService.getAllMovies();
+        else movies = movieService.getMoviesByAgeLimit(age);
+
         if (movies.isEmpty()) return ResponseEntity.noContent().build();
         else return ResponseEntity.ok(movies);
     }
-
-@GetMapping("/movies") //Movies under certain age
-    public ResponseEntity<Set<MovieDto>> getMoviesByAge(@RequestParam int age) {
-        Set<MovieDto> movies = movieService.getMoviesByAgeLimit(age);
-        if (movies.isEmpty()) return ResponseEntity.noContent().build();
-        else return ResponseEntity.ok(movies);
-    }
-
 
 }
