4eef8b12e03a3d926b58e5a87f4ebdd6dcee968e
Chris <chye0001@stud.kea.dk>
2024-10-03 09:49:56 +0200

    changed return type for methods in ShowingService to RequestShowings

--- src/main/java/godevenner/cinemabackend/showing/ShowingService.java
+++ src/main/java/godevenner/cinemabackend/showing/ShowingService.java
@@ -4,6 +4,6 @@ import java.util.List;
 import java.util.Set;
 
 public interface ShowingService {
-    List<RequestAllShowings> getAllShowings();
-    Set<RequestShowingByMovieTitle> getAllShowingsByMovieTitle(String movieTitle);
+    List<RequestShowings> getAllShowings();
+    Set<RequestShowings> getAllShowingsByMovieTitle(String movieTitle);
 }
