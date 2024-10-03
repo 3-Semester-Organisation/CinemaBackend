b551fa954ba396de80f90f5b110aa93206c9a8c7
Lars <144362461+laha0006@users.noreply.github.com>
2024-10-04 00:52:16 +0200

    refactor import

--- src/main/java/godevenner/cinemabackend/showing/dto/RequestShowings.java
+++ src/main/java/godevenner/cinemabackend/showing/dto/RequestShowings.java
@@ -1,7 +1,7 @@
 package godevenner.cinemabackend.showing.dto;
 
 import godevenner.cinemabackend.movie.Movie;
-import godevenner.cinemabackend.theatre.Theatre;
+import godevenner.cinemabackend.theatre.model.Theatre;
 
 import java.time.LocalDateTime;
 
