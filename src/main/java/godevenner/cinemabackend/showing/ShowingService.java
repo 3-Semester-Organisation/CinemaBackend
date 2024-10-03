3bc6e9e45f4eaf3e243e4caec8afa7757e102b05
Chris <chye0001@stud.kea.dk>
2024-10-03 11:05:42 +0200

    added createShowing method to ShowingService

--- src/main/java/godevenner/cinemabackend/showing/ShowingService.java
+++ src/main/java/godevenner/cinemabackend/showing/ShowingService.java
@@ -6,4 +6,6 @@ import java.util.Set;
 public interface ShowingService {
     List<RequestShowings> getAllShowings();
     Set<RequestShowings> getAllShowingsByMovieTitle(String movieTitle);
+
+    RequestShowings createShowing(PostShowing showing);
 }
