5c5729ff144b83ce0d3bf4dcc53fec96062daf12
Chris <chye0001@stud.kea.dk>
2024-10-03 09:07:02 +0200

    added 2 methods getAllShowings and getAllShowingsByMovieTitle in ShowingService interface

--- src/main/java/godevenner/cinemabackend/showing/ShowingService.java
+++ src/main/java/godevenner/cinemabackend/showing/ShowingService.java
@@ -1,4 +1,9 @@
 package godevenner.cinemabackend.showing;
 
+import java.util.List;
+import java.util.Set;
+
 public interface ShowingService {
+    List<RequestAllShowings> getAllShowings();
+    Set<RequestShowingByMovieTitle> getAllShowingsByMovieTitle(String movieTitle);
 }
