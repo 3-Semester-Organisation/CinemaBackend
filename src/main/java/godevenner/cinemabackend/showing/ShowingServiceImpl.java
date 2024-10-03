fdfdd8c1f108abcbb7d6efa8c04d3b9afd2253bd
Chris <chye0001@stud.kea.dk>
2024-10-03 09:07:27 +0200

    implemented methods from the ShowingService interface

--- src/main/java/godevenner/cinemabackend/showing/ShowingServiceImpl.java
+++ src/main/java/godevenner/cinemabackend/showing/ShowingServiceImpl.java
@@ -7,20 +7,23 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
-public class ShowingServiceImpl {
+public class ShowingServiceImpl implements ShowingService{
 
     private final ShowingRepository showingRepository;
+    private final ShowingService showingService;
     private final RequestShowingByMovieTitleMapper requestShowingByMovieTitleMapper;
     private final RequestAllShowingsMapper requestAllShowingsMapper;
 
-    public ShowingServiceImpl(ShowingRepository showingRepository,
+    public ShowingServiceImpl(ShowingRepository showingRepository, ShowingService showingService,
                               RequestShowingByMovieTitleMapper requestShowingByMovieTitleMapper,
                               RequestAllShowingsMapper requestAllShowingsMapper) {
         this.showingRepository = showingRepository;
+        this.showingService = showingService;
         this.requestShowingByMovieTitleMapper = requestShowingByMovieTitleMapper;
         this.requestAllShowingsMapper = requestAllShowingsMapper;
     }
 
+    @Override
     public Set<RequestShowingByMovieTitle> getAllShowingsByMovieTitle(String movieTitle) {
         Set<Showing> showingSet = showingRepository.getAllByMovieTitle(movieTitle);
         return showingSet.stream()
@@ -28,6 +31,7 @@ public class ShowingServiceImpl {
                 .collect(Collectors.toSet());
     }
 
+    @Override
     public List<RequestAllShowings> getAllShowings() {
         List<Showing> showingList = showingRepository.findAll();
         return showingList.stream()
