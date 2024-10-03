15a8bf7b6d3e6d9726e2f7f6ec5d71107f010b21
Chris Ye <chye0001@kea.stud.dk>
2024-10-03 12:27:10 +0200

    fixed method

--- src/main/java/godevenner/cinemabackend/showing/ShowingServiceImpl.java
+++ src/main/java/godevenner/cinemabackend/showing/ShowingServiceImpl.java
@@ -52,6 +52,6 @@ public class ShowingServiceImpl implements ShowingService{
     public boolean doesExist(PostShowing showing) {
         Showing needsCheck = postShowingMapper.apply(showing);
 
-        return showingRepository.existsByTheatreAndMovieAndStartTimeAndCancelled(needsCheck);
+        return showingRepository.existsByTheatreAndMovieAndStartTime(needsCheck.getTheatre(), needsCheck.getMovie(), needsCheck.getStartTime());
     }
 }
