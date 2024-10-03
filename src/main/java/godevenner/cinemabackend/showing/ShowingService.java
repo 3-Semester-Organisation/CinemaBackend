2a12a5ddbefba5f94b628fa1aac0857627ebc13f
Chris <chye0001@stud.kea.dk>
2024-10-03 08:58:18 +0200

    created ShowingService interface

--- src/main/java/godevenner/cinemabackend/showing/ShowingService.java
+++ src/main/java/godevenner/cinemabackend/showing/ShowingService.java
@@ -1,37 +1,4 @@
 package godevenner.cinemabackend.showing;
 
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
-
-@Service
-public class ShowingService {
-
-    private final ShowingRepository showingRepository;
-    private final RequestShowingByMovieTitleMapper requestShowingByMovieTitleMapper;
-    private final RequestAllShowingsMapper requestAllShowingsMapper;
-
-    public ShowingService(ShowingRepository showingRepository,
-                          RequestShowingByMovieTitleMapper requestShowingByMovieTitleMapper,
-                          RequestAllShowingsMapper requestAllShowingsMapper) {
-        this.showingRepository = showingRepository;
-        this.requestShowingByMovieTitleMapper = requestShowingByMovieTitleMapper;
-        this.requestAllShowingsMapper = requestAllShowingsMapper;
-    }
-
-    public Set<RequestShowingByMovieTitle> getAllShowingsByMovieTitle(String movieTitle) {
-        Set<Showing> showingSet = showingRepository.getAllByMovieTitle(movieTitle);
-        return showingSet.stream()
-                .map(requestShowingByMovieTitleMapper)
-                .collect(Collectors.toSet());
-    }
-
-    public List<RequestAllShowings> getAllShowings() {
-        List<Showing> showingList = showingRepository.findAll();
-        return showingList.stream()
-                .map(requestAllShowingsMapper)
-                .collect(Collectors.toList());
-    }
+public interface ShowingService {
 }
