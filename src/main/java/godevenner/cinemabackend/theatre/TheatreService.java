6172d59766f8ab5c1853798c121c93cb44a0d3b9
Lars <144362461+laha0006@users.noreply.github.com>
2024-10-04 03:45:46 +0200

    added two methods to interface for getting Theatre Info
    getSeatsByTheatreId,
    getLayoutsByTheatreId

--- src/main/java/godevenner/cinemabackend/theatre/TheatreService.java
+++ src/main/java/godevenner/cinemabackend/theatre/TheatreService.java
@@ -1,4 +1,12 @@
 package godevenner.cinemabackend.theatre;
 
+import godevenner.cinemabackend.theatre.model.TheatreLayout;
+import godevenner.cinemabackend.theatre.model.TheatreSeat;
+
+import java.util.Set;
+
 public interface TheatreService {
+
+    Set<TheatreSeat> getSeatsByTheatreId(long id);
+    Set<TheatreLayout> getLayoutsByTheatreId(long id);
 }
