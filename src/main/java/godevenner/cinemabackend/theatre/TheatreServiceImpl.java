be13cf7940cbc360322d8415605c11053029a455
Lars <144362461+laha0006@users.noreply.github.com>
2024-10-04 03:46:09 +0200

    implemented the two methods
    getSeatsByTheatreId
    getLayoutsByTheatreId

--- src/main/java/godevenner/cinemabackend/theatre/TheatreServiceImpl.java
+++ src/main/java/godevenner/cinemabackend/theatre/TheatreServiceImpl.java
@@ -1,4 +1,37 @@
 package godevenner.cinemabackend.theatre;
 
+import godevenner.cinemabackend.theatre.model.Theatre;
+import godevenner.cinemabackend.theatre.model.TheatreLayout;
+import godevenner.cinemabackend.theatre.model.TheatreSeat;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.Set;
+
+@RequiredArgsConstructor
+@Service
 public class TheatreServiceImpl implements TheatreService {
+
+    private final TheatreRepository theatreRepository;
+
+    @Override
+    public Set<TheatreSeat> getSeatsByTheatreId(long id) {
+        Optional<Theatre> theatreOptional = theatreRepository.findById(id);
+        if (theatreOptional.isPresent()) {
+            Theatre theatre = theatreOptional.get();
+            return theatre.getTheatreSeats();
+        }
+        throw new RuntimeException("There is no theatre with id " + id);
+    }
+
+    @Override
+    public Set<TheatreLayout> getLayoutsByTheatreId(long id) {
+        Optional<Theatre> theatreOptional = theatreRepository.findById(id);
+        if (theatreOptional.isPresent()) {
+            Theatre theatre = theatreOptional.get();
+            return theatre.getTheatreLayouts();
+        }
+        throw new RuntimeException("There is no theatre with id " + id);
+    }
 }
