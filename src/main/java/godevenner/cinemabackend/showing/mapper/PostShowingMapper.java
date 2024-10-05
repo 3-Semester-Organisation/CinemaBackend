10384a9f0d296731a35741fe74ac9e0677d64c56
Lars <144362461+laha0006@users.noreply.github.com>
2024-10-05 03:38:46 +0200

    refactor

--- src/main/java/godevenner/cinemabackend/showing/mapper/PostShowingMapper.java
+++ src/main/java/godevenner/cinemabackend/showing/mapper/PostShowingMapper.java
@@ -2,7 +2,7 @@ package godevenner.cinemabackend.showing.mapper;
 
 import godevenner.cinemabackend.movie.Movie;
 import godevenner.cinemabackend.movie.MovieRepository;
-import godevenner.cinemabackend.showing.Showing;
+import godevenner.cinemabackend.showing.model.Showing;
 import godevenner.cinemabackend.showing.dto.PostShowing;
 import godevenner.cinemabackend.theatre.model.Theatre;
 import godevenner.cinemabackend.theatre.TheatreRepository;
