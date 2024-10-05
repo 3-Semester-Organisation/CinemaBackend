c171a1fef5e612a7ca22e6b94fa27830a861d8ed
Chris <chye0001@stud.kea.dk>
2024-10-05 15:18:38 +0200

    made costum query for finding the latestShow

--- src/main/java/godevenner/cinemabackend/showing/ShowingRepository.java
+++ src/main/java/godevenner/cinemabackend/showing/ShowingRepository.java
@@ -3,11 +3,18 @@ package godevenner.cinemabackend.showing;
 import godevenner.cinemabackend.movie.Movie;
 import godevenner.cinemabackend.theatre.Theatre;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 public interface ShowingRepository extends JpaRepository<Showing, Long> {
     Set<Showing> getAllByMovieId(long id);
     boolean existsByTheatreAndStartTime(Theatre theatre, LocalDateTime startTime);
+
+
+    @Query("SELECT s FROM Showing s WHERE s.theatre.id = :theatreId ORDER BY s.startTime DESC")
+    Optional<List<Showing>> findLatestShowingByTheatreId(long theatreId);
 }
