5678cf58db428841e59b22946e1b94b31e4a562f
Chris <chye0001@stud.kea.dk>
2024-10-08 11:03:36 +0200

    wrapped returntype for methods in UserRepository in optinals

--- src/main/java/godevenner/cinemabackend/user/UserRepository.java
+++ src/main/java/godevenner/cinemabackend/user/UserRepository.java
@@ -2,7 +2,9 @@ package godevenner.cinemabackend.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-    User findByEmail(String email);
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
 }
