aa7c59a74dcc9cf7f530d213a707d25f2f597484
Chris <chye0001@stud.kea.dk>
2024-10-08 13:59:44 +0200

    created ApplicationConfiguration containing beans, used for authentication users in AuthServiceImpl

--- src/main/java/godevenner/cinemabackend/config/ApplicationConfiguration.java
+++ src/main/java/godevenner/cinemabackend/config/ApplicationConfiguration.java
@@ -4,9 +4,14 @@ import godevenner.cinemabackend.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 //this class is not really needed since UserDetailsService already has a method of returning a user by the username.
@@ -17,12 +22,27 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 public class ApplicationConfiguration {
 
     private final UserRepository userRepository;
-
+    private final PasswordEncoder passwordEncoder;
 
     //this bean is used to implement own costume fetching of user from the db.
     @Bean
     public UserDetailsService userDetailsService() {
         return username -> userRepository.findByUsername(username) //change this to findByEmail or phoneNumber or any other ways to find a user.
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+
+        //we could have multiple implementations of userDetailsService, so here we specify which one to fetch from.
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder);
+        return authProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 }
