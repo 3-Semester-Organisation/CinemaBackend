01cb13649aa2ac6b45b9c908c5d305180f9b70ae
Chris <chye0001@stud.kea.dk>
2024-10-08 10:29:23 +0200

    created AuthenticationController

---
+++ src/main/java/godevenner/cinemabackend/login/AuthenticationController.java
@@ -0,0 +1,11 @@
+package godevenner.cinemabackend.login;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class AuthenticationController {
+
+
+}
