973f17b3970646d5bafa9419fa18babdfa47d875
Christoffer <chri26eq@stud.kea.dk>
2024-10-08 13:32:53 +0200

    import fjernet

--- src/main/java/godevenner/cinemabackend/movie/service/MovieServiceImpl.java
+++ src/main/java/godevenner/cinemabackend/movie/service/MovieServiceImpl.java
@@ -12,7 +12,6 @@ import org.springframework.stereotype.Service;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
