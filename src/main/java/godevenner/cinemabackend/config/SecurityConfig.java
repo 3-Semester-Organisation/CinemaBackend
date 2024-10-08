6a86410c58e2549f9d4b5932a83256d15474a29c
Chris Ye <chye0001@kea.stud.dk>
2024-10-08 23:10:33 +0200

    broke circular dependency by removing @Bean passwordEncoder from SecurityConfig to ApplicationConfig

--- src/main/java/godevenner/cinemabackend/config/SecurityConfig.java
+++ src/main/java/godevenner/cinemabackend/config/SecurityConfig.java
@@ -68,9 +68,4 @@ public class SecurityConfig {
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 }
