c4dea08344de92c08ed28d723ffc127e2006e651
Lars <144362461+laha0006@users.noreply.github.com>
2024-10-09 06:37:03 +0200

    commented out not needed.

--- src/main/java/godevenner/cinemabackend/booking/mapper/BookingRequestMapper.java
+++ src/main/java/godevenner/cinemabackend/booking/mapper/BookingRequestMapper.java
@@ -1,37 +1,19 @@
-package godevenner.cinemabackend.booking.mapper;
-
-import godevenner.cinemabackend.booking.dto.BookingRequest;
-import godevenner.cinemabackend.booking.model.Booking;
-import org.springframework.stereotype.Component;
-
-import java.util.function.Function;
-
-// this bit is only here to not crash the program
-@Component
-public class BookingRequestMapper {
-
-
-    public Booking apply(BookingRequest booking) {
-        return null;
-    }
-}
-
-
-// commented out for now, can't call booking.getBookedSeats()
-/*
-@Component
-public class BookingRequestMapper implements Function<Booking, BookingRequest> {
-
-    @Override
-    public BookingRequest apply(Booking booking) {
-        return new BookingRequest(
-                booking.getShowing(),
-                booking.getCustomer(),
-                booking.getBookedSeats()
-        );
-    }
-
- */
+//package godevenner.cinemabackend.booking.mapper;
+//
+//import godevenner.cinemabackend.booking.dto.BookingRequest;
+//import godevenner.cinemabackend.booking.model.Booking;
+//import org.springframework.stereotype.Component;
+//
+//import java.util.function.Function;
+//
+//// this bit is only here to not crash the program
+//@Component
+//public class BookingRequestMapper implements Function<BookingRequest, Booking> {
+//    @Override
+//    public Booking apply(BookingRequest bookingRequest) {
+//        return new Booking()
+//    }
+//}
 
 
 
