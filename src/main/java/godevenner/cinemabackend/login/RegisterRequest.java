0e59767d3768945778734b50aa339dc6c133d646
Chris Ye <chye0001@kea.stud.dk>
2024-10-10 11:44:20 +0200

    added following attributes: fullName, phoneNumber, email, birthDate.

--- src/main/java/godevenner/cinemabackend/login/RegisterRequest.java
+++ src/main/java/godevenner/cinemabackend/login/RegisterRequest.java
@@ -1,8 +1,14 @@
 package godevenner.cinemabackend.login;
 
+import java.time.LocalDate;
+
 public record RegisterRequest(
         String username,
-        String password
+        String password,
+        String fullName,
+        String phoneNumber,
+        String email,
+        LocalDate birthDate
         //add other information such as first- and last name, birthday and phone number, email
 ) {
 }
